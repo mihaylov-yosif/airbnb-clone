e117466498c98968531a64f793e2a091963a443a
Yosif Mihaylov <yosifmihaylov.dev@gmail.com>
2023-05-03 16:26:28 +0300

    Sign up UI
    Add Sign up components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from '@/providers/ToasterProvider'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -17,6 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal isOpen title="Hello World"/>
         <Navbar />
         {children}
       </body>
